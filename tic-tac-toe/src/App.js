c0507f1dcf375f050e83a5d7d1ce480c10f451ee
Sowmy <sowmywayne@Sowmys-MacBook-Air.local>
2020-05-08 00:04:28 +0530

    using useState created (Tic-Tac-Toe)

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+
+import Icon from "./components/Icon";
+import { ToastContainer, toast } from "react-toastify";
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+
+import "react-toastify/dist/ReactToastify.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css";
+
+const itemArray = new Array(9).fill("empty");
+
+
+
+const App = () => {
+  const [isCross, setIsCross] = useState(false);
+const [winMessage, setWinMessage] = useState("");
+
+const reloadGame = () => {
+  setIsCross(false)
+  setWinMessage("")
+  itemArray.fill("empty", 0, 9)
+};
+
+const checkWinner = () => {
+  if (
+    itemArray[0] === itemArray[1] && 
+    itemArray[0] === itemArray[2] && 
+    itemArray[0] !== 'empty') {
+    setWinMessage(`${itemArray[0]} is wins`)
+  }
+  else if (
+    itemArray[3] === itemArray[4]&&
+    itemArray[3] === itemArray[5]&&
+    itemArray[3] !== 'empty'
+    ){
+      setWinMessage(`${itemArray[3]} is wins`)
+    }
+  else if (
+    itemArray[6] === itemArray[7]&&
+    itemArray[6] === itemArray[8]&&
+    itemArray[6] !== 'empty'
+    ){
+      setWinMessage(`${itemArray[6]} is wins`)
+    }
+  else if (
+    itemArray[0] === itemArray[3]&&
+    itemArray[0] === itemArray[6]&&
+    itemArray[0] !== 'empty'
+    ){
+      setWinMessage(`${itemArray[0]} is wins`)
+    }
+  else if (
+    itemArray[1] === itemArray[4]&&
+    itemArray[1] === itemArray[7]&&
+    itemArray[1] !== 'empty'
+    ){
+      setWinMessage(`${itemArray[1]} is wins`)
+    }
+  else if (
+    itemArray[2] === itemArray[6]&&
+    itemArray[2] === itemArray[8]&&
+    itemArray[2] !== 'empty'
+    ){
+      setWinMessage(`${itemArray[2]} is wins`)
+    }
+  else if (
+    itemArray[0] === itemArray[4]&&
+    itemArray[0] === itemArray[8]&&
+    itemArray[0] !== 'empty'
+    ){
+      setWinMessage(`${itemArray[0]} is wins`)
+    }  
+  else if (
+    itemArray[2] === itemArray[4]&&
+    itemArray[2] === itemArray[6]&&
+    itemArray[2] !== 'empty'
+    ){
+      setWinMessage(`${itemArray[2]} is wins`)
+    }  
+};
+
+const changeItem = itemNumber => {
+  if(winMessage) {
+    return toast(winMessage, {type: 'success'})
+  }
+
+  if (itemArray[itemNumber] === "empty") {
+    itemArray[itemNumber] = isCross ? "cross": "circle"
+    setIsCross(!isCross)
+
+  }else {
+    return toast("already filled", {type: 'warning'})
+  }
+
+  checkWinner();
+
+};
+  
+
+  return (
+    <Container className="p-5">
+      <ToastContainer position="bottom-center"></ToastContainer>
+      <Row>
+        <Col md={6} className="offset-md-3">
+          {winMessage ? (
+            <div className="mb-2 mt-2">
+              <h1 className="text-primary text-uppercase text-center">
+                {winMessage}
+              </h1>
+              <Button color="success" block onClick={reloadGame}>Reload the game</Button>
+            </div>
+          ): (
+            <h1 className="text-center text-warning">
+              {isCross ? "Cross" : "Circle"} turns
+            </h1>
+          )} 
+          <div className="grid">
+            {itemArray.map((item, index) => (
+              <Card color = "warning" onClick={() => {changeItem(index)}}>
+                <CardBody className="box">
+                  <Icon name={item}></Icon>
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default App;
