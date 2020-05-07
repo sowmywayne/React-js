f4577407fa99e4e23f3079544710eef9c9a2a02d
Sowmy <sowmywayne@Sowmys-MacBook-Air.local>
2020-05-08 00:07:58 +0530

    using Create and Use Context created(ThemeToggler)

---
+++ context/src/Components/ThemeToggler.js
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import ThemeContext from '../Context/ThemeContext'
+
+
+const ThemeToggler = () => {
+  const [themeMode, setThemeMode] = useContext(ThemeContext)
+
+  return (
+    <div 
+    onClick={() => {
+      setThemeMode(themeMode === "light" ? "dark": "light")
+    }}>
+      <span>
+        {themeMode === "light"? "Turn off": "ligth on"}
+      </span>
+
+    </div>
+  )
+}
+
+
+export default ThemeToggler;
