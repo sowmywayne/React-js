a6fe3d0d9c3a4630eb84bbca949940ab3305fecb
Sowmy <sowmywayne@Sowmys-MacBook-Air.local>
2020-05-08 00:12:55 +0530

    Using context create and provider to pass props much easier

---
+++ context sharing props/src/App.js
@@ -0,0 +1,50 @@
+import React, {Fragment} from 'react'
+import Provider from './provider'
+import Context from './context'
+
+
+const Agents = () => {
+  return <AgentOne />
+}
+
+
+const AgentOne = () => {
+  return <AgentTwo />
+}
+
+const AgentTwo = () => {
+  return <AgentBond />
+}
+
+
+const AgentBond = () => {
+  return(
+    <Context.Consumer>
+      {
+        (context) => (
+          <Fragment>
+            <h3>Agent information</h3>
+        <p>Mission Name: {context.data.mname}</p>
+        <h2>Mission status: {context.data.accept}</h2>
+        <button onClick={context.isMissionAccepted}>Choose to Accept</button>
+          </Fragment>
+        )
+      }
+    </Context.Consumer>
+  )
+}
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Context Api</h1>
+      <Provider>
+        <Agents />
+      </Provider>
+    </div>
+  )
+}
+
+
+export default App;
